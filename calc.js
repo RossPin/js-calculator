f07a03a7d1b96c5ea0c0bac45e125a32b5ca66fa
RossPin <dorpetz@hotmail.com>
2018-03-15 12:06:14 +1300

    make init & addListener functions

--- calc.js
+++ calc.js
@@ -5,11 +5,22 @@ var entries = [];
 var result;
 
 function init() {
-
+  addListener('btnAC', clearAll());
+  addListener('btnCE', clearLast());
+  for (var i = 0; i <= 9; i++) {
+    addListener('btn'+i, addNumber(i));
+  }
+  addListener('btnDec', addNumber('.'));
+  addListener('btnAdd', addOperand('+'));
+  addListener('btnSub', addOperand('-'));
+  addListener('btnMlt', addOperand('*'));
+  addListener('btnDiv', addOperand('/'));
+  addListener('btnEq', equals());
+  updateDisplay();
 }
 
-function addListeners () {
-
+function addListener (btn, func) {
+  document.getElementById(btn).addEventListener('click', func);
 }
 
 function addNumber(num) {
@@ -25,9 +36,17 @@ function equals() {
 }
 
 function calculate () {
-  
+
 }
 
 function updateDisplay () {
+
+}
+
+function clearAll() {
+
+}
+
+function clearLast() {
 
 }
