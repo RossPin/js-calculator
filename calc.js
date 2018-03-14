6df4cb7240217ac853f8624733332c4125c7bde2
RossPin <dorpetz@hotmail.com>
2018-03-15 11:50:58 +1300

    add empty functions

---
+++ calc.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', init)
+
+var input = "";
+var entries = [];
+var result;
+
+function init() {
+
+}
+
+function addListeners () {
+
+}
+
+function addNumber(num) {
+
+}
+
+function addOperand(op) {
+
+}
+
+function equals() {
+
+}
+
+function calculate () {
+  
+}
+
+function updateDisplay () {
+
+}
